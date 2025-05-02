2bad94df5077da2a9a381b2ca68e0d05e7d9aecc
woojin0421 <woojin4693@naver.com>
2025-05-02 12:24:39 +0900

    .

---
+++ tapie 과제/js/main.js
@@ -0,0 +1,17 @@
+function webtoonlogo(){
+    window.location.href = "index.html";
+}
+function webtoon1(){
+    window.location.href = "webtoon1.html";
+}
+function banner(){
+    let image = document.getElementById('netflix');
+    let source = "https://mkt.naver.com/netflix?dtm_source=wt_tripleplay&dtm_medium=mktatrb_wt&dtm_campaign=2504-mem-001&pcode=wt_tripleplay&campaign_id=2504-mem-001&channel_id=wt_tripleplay";
+    window.open(source);
+}
+function webtoon2(){
+    window.location.href = "webtoon2.html";
+}
+function webtoon4(){
+    window.location.href = "webtoon4.html";
+}
